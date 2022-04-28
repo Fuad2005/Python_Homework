387143c0194021ecfe47a7e87d7998816da23be6
Fuad2005 <fuadqasimov000@gmail.com>
2022-04-28 17:36:17 +0400

    python

---
+++ homework_1.py
@@ -0,0 +1,33 @@
+#1)
+card= '5382-1739-9201-9017'
+print(card[:9].ljust(19,'*'))
+
+#2)
+print((15%4)**3)
+
+#3)
+print(round(256.91872,2))
+
+#4)
+print(str(34)[:2].zfill(5))
+
+#5)
+#input('number: ')
+#???
+
+
+#6)
+site=input('website: ')
+no_https=site.removeprefix('https://')
+domain=no_https.removesuffix('.com')
+print(domain)
+
+# #7)
+# metn='''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel vestibulum magna, nec scelerisque odio. Duis at pellentesque est. Vestibulum ac interdum quam. Mauris ac lorem vel dolor accumsan lobortis. Aenean ut hendrerit arcu. Quisque sagittis in ipsum quis accumsan. Maecenas placerat mi ac tellus convallis, ac gravida risus eleifend. Duis velit sapien, tristique quis mauris id, facilisis laoreet tellus.
+
+# Sed vehicula sapien in volutpat blandit. Mauris fermentum est in arcu porttitor ornare. Proin consequat ante non elementum accumsan. Sed lacus felis, elementum sodales nisi sit amet, convallis laoreet sapien. Nulla facilisi. In dictum, purus in pharetra mollis, purus lectus aliquam tortor, in aliquam eros dolor vel elit. Pellentesque quis tempus magna, sit amet imperdiet augue. Morbi interdum faucibus magna, vitae ullamcorper ante fringilla vitae. Vestibulum semper tortor ligula, at porttitor orci posuere id. Curabitur tempor ligula ut libero feugiat finibus non in nulla. Sed quam ex, auctor a tempor consectetur, facilisis sed elit. Sed commodo, lectus nec mattis dignissim, erat ex tincidunt tortor, quis pharetra purus ex eget ex. Pellentesque bibendum odio ut turpis commodo, ut malesuada odio rhoncus. Integer laoreet odio quis pulvinar finibus.
+
+# Aliquam sem massa, pellentesque non malesuada nec, pellentesque a lectus. Nam semper magna a nunc fringilla commodo. Ut dictum eleifend massa, non rutrum ex porttitor quis. In placerat ipsum placerat, mollis magna et, molestie odio. Nulla leo metus, blandit ac rhoncus at, posuere eu leo. Nulla facilisi. Nam venenatis egestas massa et mollis.
+
+# Phasellus placerat ornare consectetur. Nulla eget tortor vel justo congue molestie et ut felis. Duis nisi elit, pellentesque id iaculis quis, interdum eget lorem. Sed tempor metus a velit tempus sagittis. Proin at dui augue. Cras quis erat ut ligula gravida ultricies vitae vel mauris. Curabitur porttitor auctor mi sed rutrum. Duis vel porta elit. Integer eu rutrum ipsum. Nam ut.'''
+#???

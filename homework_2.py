6434cec84db27e961e1776e09b532735fdc808fc
Fuad2005 <fuadqasimov000@gmail.com>
2022-04-30 17:12:06 +0400

    Second Homework

---
+++ homework_2.py
@@ -0,0 +1,38 @@
+#1)
+# number=int(input('Number\n>>> '))
+# result=1
+# while number>1 :
+#     result=result*number
+#     number=number-1
+# print(' ' + str(number) + '! ='+ str(result))
+
+
+#2)
+# sentence= input('sentence\n>>> ')
+# vowels='euioa'
+# counter=0
+# result=0
+# while True:
+#     letter=sentence[counter]
+#     result+=(1 if letter in vowels else 0)
+#     counter+=1
+#     if counter==len(sentence):
+#         break
+# print(result)
+
+
+#3)
+#???
+
+
+#4)
+# a = '123456789'
+# print(*a[::-1], sep=' saniye\n',end='Vaxt Bitti!')
+
+
+#5)
+#???
+
+
+#6)
+#???

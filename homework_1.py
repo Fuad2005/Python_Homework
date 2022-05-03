d84c09f806b0c8bcf734a591b177fc957226464f
Fuad2005 <fuadqasimov000@gmail.com>
2022-05-03 13:49:55 +0400

    First Homework

--- homework_1.py
+++ homework_1.py
@@ -1,15 +1,15 @@
 # 1)
-card = '5382-1739-9201-9017'
-print(card[:9].ljust(19, '*'))
+# card = '5382-1739-9201-9017'
+# print(card[:9].ljust(19, '*'))
 
 # 2)
-print((15 % 4)**3)
+# print((15 % 4)**3)
 
 # 3)
-print(round(256.91872, 2))
+# print(round(256.91872, 2))
 
 # 4)
-print(str(34)[:2].zfill(5))
+# print(str(34)[:2].zfill(5))
 
 # 5)
 #input('number: ')
@@ -17,12 +17,12 @@ print(str(34)[:2].zfill(5))
 
 
 # 6)
-site = input('website: ')
-no_https = site.removeprefix('https://')
-domain = no_https.removesuffix('.com')
-print(domain)
+# site = input('website: ')
+# no_https = site.removeprefix('https://')
+# domain = no_https.removesuffix('.com')
+# print(domain)
 
-# #7)
+#7)
 # metn='''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel vestibulum magna, nec scelerisque odio. Duis at pellentesque est. Vestibulum ac interdum quam. Mauris ac lorem vel dolor accumsan lobortis. Aenean ut hendrerit arcu. Quisque sagittis in ipsum quis accumsan. Maecenas placerat mi ac tellus convallis, ac gravida risus eleifend. Duis velit sapien, tristique quis mauris id, facilisis laoreet tellus.
 
 # Sed vehicula sapien in volutpat blandit. Mauris fermentum est in arcu porttitor ornare. Proin consequat ante non elementum accumsan. Sed lacus felis, elementum sodales nisi sit amet, convallis laoreet sapien. Nulla facilisi. In dictum, purus in pharetra mollis, purus lectus aliquam tortor, in aliquam eros dolor vel elit. Pellentesque quis tempus magna, sit amet imperdiet augue. Morbi interdum faucibus magna, vitae ullamcorper ante fringilla vitae. Vestibulum semper tortor ligula, at porttitor orci posuere id. Curabitur tempor ligula ut libero feugiat finibus non in nulla. Sed quam ex, auctor a tempor consectetur, facilisis sed elit. Sed commodo, lectus nec mattis dignissim, erat ex tincidunt tortor, quis pharetra purus ex eget ex. Pellentesque bibendum odio ut turpis commodo, ut malesuada odio rhoncus. Integer laoreet odio quis pulvinar finibus.

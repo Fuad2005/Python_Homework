a62dbf33f5f978d7d0a79f16ff3cf615864bc8f9
Fuad2005 <fuadqasimov000@gmail.com>
2022-05-05 14:04:38 +0400

    Forth Homework

---
+++ homework_4.py
@@ -0,0 +1,46 @@
+#1)
+# word= input('soz: ')
+# amount= input('say: ')
+# print(word*int(amount))
+
+
+#2)
+# fruits=['alma', 'armud', 'heyva', 'nar', 'saftali']
+# friut_count = [[i, len(i)] for i in fruits]
+# print(friut_count)
+
+
+#3)
+#???
+
+
+#4)
+# numbers=[]
+# for i in range(-100,100):
+#     if not i%7:
+#         numbers.append(i)
+# print(numbers)
+
+#5)
+# fruits=['alma', 'armud', 'heyva', 'nar', 'saftali']
+# options = {'key': len, 'reverse': True}
+# fruits.sort(**options)
+# print(fruits)
+
+
+#6)
+# user_info = {
+#     'firstname': input('FirstName: '),
+#     'lastname': input('Lastname: '),
+#     'username': input('Username: '),
+#     'password': input('Password: '),
+#     'birthday': input('Birth Date: ')
+# }
+# print(
+#     '\n\n\nFirstName: '+ user_info['firstname'],
+#     'LastName: '+ user_info['lastname'],
+#     'UserName: '+ user_info['username'],
+#     'Password: '+ user_info['password'],
+#     'Birth Date: '+ user_info['birthday'],
+#     sep='\n'
+# )

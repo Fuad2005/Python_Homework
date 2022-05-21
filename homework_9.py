892b1acb17b0b7ea3b1f0234e0931e6a7608486b
Fuad2005 <fuadqasimov000@gmail.com>
2022-05-21 16:33:28 +0400

    Ninth Homework

---
+++ homework_9.py
@@ -0,0 +1,38 @@
+#1)
+# physic = {
+# 	'F': lambda m, a: m * a,
+# 	'Ep': lambda m,g,h: m*g*h,
+#     'Ek': lambda m,v: (m*v**2)/2,
+#     'R': lambda *args: sum(args),
+#     'a': lambda v1,v2,t1,t2: (v2-v1)/(t2-t1),
+# 	'T': lambda f: 1/f,
+#     's': lambda a,t: (a*t**2)/2,
+#     'Ew': lambda W,Q: W+Q,
+# 	'W': lambda U,I,t: U*I*t,
+#     'p': lambda Fn,S: Fn/S
+
+
+
+
+# }
+# print('F =',physic.get('F')(5, 4))
+# print('Ep =',physic.get('Ep')(3, 7,6))
+# print('Ek =',physic.get('Ek')(1, 4))
+# print('R =',physic.get('R')(5, 4,1,5,6,7,4,6,44))
+# print('a =',physic.get('a')(7,16, 4,12))
+# print('T =',physic.get('T')(5))
+# print('s =',physic.get('s')(10,5))
+# print('Ew =',physic.get('Ew')(4,12))
+# print('W =',physic.get('W')(8,7,2))
+# print('p =',physic.get('p')(16,12))
+
+
+
+#2)
+# factorial=lambda num: 1 if num==1 else num*factorial(num-1)
+# print(factorial(5))
+
+
+
+#3)
+#???????

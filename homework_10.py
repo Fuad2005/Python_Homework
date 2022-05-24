24e0ff34828dea2e9ffdf7867e69da670e97bb8d
Fuad2005 <fuadqasimov000@gmail.com>
2022-05-24 14:33:53 +0400

    Tenth Homework

---
+++ homework_10.py
@@ -0,0 +1,72 @@
+baliqlar = {
+    'qelseme teneffusu', '2 kamerali urek', 'uzgec', 'korteks yoxdur',
+    'yumurtlama', 'dis yoxdur', '4 ayaq'
+}
+
+cuculer = {
+    'toraks teneffusu', 'urek yoxdur', '6 ayaq',
+    'korteks vardir', 'beyin yoxdur', 'yumurtlama', 'metamorfoz', 
+    'dis yoxdur'
+}
+
+amfibialar = {
+    'qelseme teneffusu', 'agciyer teneffusu', 'uzgec', '4 ayaq', 
+    '2 kamerali urek', '3 kamerali urek', 'metamorfoz', 'kotex vardir',
+    'yumurtlama', 'dis yoxdur'
+}
+
+surunenler = {
+    'agciyer teneffusu', '3 kamerali urek', '4 ayaq', 'korteks vardir', 'yumurtlama',
+    'dis var'
+}
+
+quslar = {
+    'agciyer teneffusu', '4 kamerali urek', 'korteks vardir',
+    'yumurtlama', 'dis yoxdur'
+}
+
+memeliler = {
+    'agciyer teneffusu', '4 kamerali urek', '4 ayaq', 'korteks vardir',
+    'dogma', 'dis vardir'
+}
+
+sinifler = {
+    'baliqlar': baliqlar,
+    'cuculer': cuculer,
+    'amfibialar': amfibialar,
+    'surunenler': surunenler,
+    'quslar': quslar,
+    'memeliler': memeliler,
+}
+
+
+#1)
+# quslar.add('2 ayaq')
+# print(quslar)
+
+
+#2)
+# sinifler['baliqlar'].remove('4 ayaq')
+# print(baliqlar)
+
+
+#3)
+# print(amfibialar.intersection(cuculer))
+
+
+#4)
+# print(sinifler['baliqlar'].symmetric_difference(sinifler['amfibialar']))
+
+
+#5)
+for key,value in sinifler.items():
+	if not baliqlar.intersection(value):
+		print(value)
+
+
+#6)
+#????????????????????
+
+
+#7)
+#????????????????????

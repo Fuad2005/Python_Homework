cf0fecea1d5190b97c6707780cbb3c91f55ff654
Fuad2005 <fuadqasimov000@gmail.com>
2022-05-26 16:03:19 +0400

    Eleventh Homework

---
+++ homework_11.py
@@ -0,0 +1,52 @@
+#1)???????
+# numbers=[85856, 73930, 95298, 57870, 99688, 92907, 13075, 12905, 52948, 97687, 10832, 78757, 99502, 65889, 34618, 59109, 83419, 31486, 94522, 34400]
+# def sum_of_digits(numlist):
+#     sum=0
+#     sum_list=[]
+#     for num in numlist:
+#         for digit in str(num):
+            
+#             sum+=int(digit)
+#         sum_list.append(str(sum))
+#         print(num,'==>',sum)
+#         sum=0
+    
+# print(sum_of_digits(numbers))
+
+
+
+#2)
+# text='''Python dili - interpretasiya olunan, yüksək səviyyəli və ümumi-məqsədli proqramlaşdırma dilidir. 1991-ci ildə Guido van Rossum tərəfindən yaradılmışdır. Python dilinin dizayn fəlsəfəsi boşluqlardan istifadə edərək kod oxunaqlılığını vurğulayır. Bu dilin məqsədi onun dil quruluşu və obyekt-yönümlülüyü ilə proqramçılara xırda və iri həcimli layihələrdə aydın, məntiqli kod yazmağa kömək etməkdir.
+
+# Python dinamik yazıla bilən və avtomatik yaddaş idarəetmə xüsusiyyətinə malikdir. Müxtəlif proqramlaşdırma paradiqmalarını dəstəkləyir, buna daxildir strukturlu (qismən, prosedurlu), obyekt-yönümlü və funksional proqramlaşdırma paradiqmaları. Onun geniş standart kitabxanası olduğuna görə Pythona "bataryaları daxildir" də deyilir.
+
+# Python 1980-ci illərin sonlarında ABC dilinin davamçısı olaraq yaradıldı. Python 2.0 2000-ci ildə təqdim edildi, o özü ilə birlikdə siyahı anlama və zibil toplayıcı sistemlə birgə referans sayıcı xüsusiyyətlərini gətirdi.
+
+# Python 3.0 2008-ci ildə təqdim edildi, o dilin böyük reviziyası olduğundan onun əvvələ-uyğunluğu tam deyil və bir çox Python 2 kodu dəyişikliyə ehtiyac olmadan Python 3-də işləyə bilmir.
+
+# Python 2 dili 2020-ci ildə rəsmi olaraq dayandırıldı (ilk dəfə 2015-ildə dayandırılması planlaşdırılmışdı) və Python 2.7.18 son Python 2.7 buraxılışı oldu və beləliklə son Python 2.7 buraxılışı da o oldu. Bunun üçün daha heç bir təhlükəsizlik yamağı yada başqa təkmilləşdirmə buraxılmayacaq. Python 2-lər üçün hər şey dayandırılıb, yalnız Python 3.6.x və daha sonrası dəstəklənir.
+
+# Bir çox əməliyyat sistemləri üçün Python interpretatorları mövcuddur. Bir qlobal proqramçılar cəmiyyəti azad və açıq-qaynaq kodlu istinad proqram olan CPythonu yazıb və inkişaf etdirir. Python və CPython-un inkişafı üçün qaynaqlari gəlir məqsədi güdməyən qurum olan Python Software Foundation idarə edir və yönləndirir.'''
+
+
+
+# def filter_words(words):
+#     vowels={
+#     'ü','e','u','i','o','ö','a','ı','ə'
+#     }
+#     for word in words:
+#         if words[0] in vowels and words[-1] in vowels:
+#             return True
+#         else:
+#             return False
+    
+# print(set(filter(filter_words,text.split(' '))))
+
+
+
+#3)
+#?????
+
+
+#4)
+#??????????????

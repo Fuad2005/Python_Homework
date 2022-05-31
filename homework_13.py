d90d4d5adddd2c0374b03baae85b647b1b0a11b6
Fuad2005 <fuadqasimov000@gmail.com>
2022-05-31 10:28:45 +0400

    Thirteenth Homework

---
+++ homework_13.py
@@ -0,0 +1,16 @@
+#1)
+# with open('numbers.txt', mode='w') as file:
+#     file.writelines([str(i).zfill(4)+'.\n' for i in range(1001)])
+
+
+#2)
+#????????????
+#faylin icinde her seyi silir,ve hecne elave etmir(
+
+# path = input('Minify etmek istediyiniz faylin adresini girin\n>>> ')
+# with open(path, mode='w') as file:
+#     str(file).replace('\n','')
+
+
+#3)
+#???????

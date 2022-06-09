5d766152859bf362a3a6e62c02c793136b9f333e
Fuad2005 <fuadqasimov000@gmail.com>
2022-06-09 11:24:22 +0400

    Seventeenth Homework

---
+++ homework_17.py
@@ -0,0 +1,42 @@
+#1)
+# import time
+
+# for i in range(int(input('Bir eded girin: '))+1,1,-1):
+#     print(i-1)
+#     time.sleep(1)
+# print('Vaxt bitti')
+
+
+import os
+#2)
+#????????????????????
+# for i in os.walk(os.path.dirname(os.path.dirname(os.getcwd()))):
+#     print(list(os.path.splitext(str(i))))
+
+import json
+import uuid
+#3)
+# user_info = [
+#     {   
+#         "full_name": "Hesen Hesenov",
+#         "username": "hasan_hasanov"
+#     },
+#     {   
+#         "full_name": "Camal Isayev",
+#         "username": "cml_iev95"
+#     },
+#     {   
+#         "full_name": "Elesger Ayazov",
+#         "username": "ea2002"
+#     },
+#     {   
+#         "full_name": "Kamal Zamanov",
+#         "username": "kml_time222"
+#     }
+# ]
+
+# for i in user_info:
+#     id = uuid.uuid4()
+#     i["id"] = str(id)
+#     print(i)
+    
